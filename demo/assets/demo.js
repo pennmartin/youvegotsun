44ccdece8d9a960d3c2a2c6b9f379d00c5c3b4b8
Keith Philpott <keith@digitalbutter.com>
2015-04-18 14:12:53 -0700

    updates to base template

--- demo/assets/demo.js
+++ demo/assets/demo.js
@@ -2,5 +2,11 @@ $(function(){
     // update status
     setTimeout(function(){
         $('#status').hide();
+        $('#score').show();
+
+        setTimeout(function(){
+
+        }, 300)
     }, 1500)
+
 });
